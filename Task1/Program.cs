6fa536d1b6e6aa4753c26cad6489a3878c246a45
Anastasia Bortnikova <nn-astel@live.com>
2024-05-20 15:16:07 +0200

    Added a simple program

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,9 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello! How many employees do you have? ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            string[] employee = new string[numberOfEmployees];
+            int[] salary = new int[numberOfEmployees];
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("Please add the name of employee: ");
+                employee[i] = Console.ReadLine();
+                Console.WriteLine("Great, now add the salary of the employee (SEK / month): ");
+                salary[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Thank you! Here is the list of employees:");
+            Console.WriteLine("Employee Salary (SEK / month)");
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine(employee[i] + " " + salary[i]);
+            }            
         }
     }
 }
